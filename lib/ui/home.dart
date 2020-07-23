2ada25b5481e8497b342f55bfae52c01da991266
zubquzaini <zubliquzaini@gmail.com>
2020-07-23 14:40:38 +0800

    BizCard App

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -24,27 +24,28 @@ class ScaffoldExample extends StatelessWidget {
           )
         ],
       ),
-
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.lightBlue,
         child: Icon(Icons.call_missed),
         onPressed: () => debugPrint("Button Pressedr"),
       ),
-
-      bottomNavigationBar: BottomNavigationBar(items: [
-        BottomNavigationBarItem(icon: Icon(Icons.account_circle), title: Text("Menu")),
-        BottomNavigationBarItem(icon: Icon(Icons.ac_unit), title: Text("Second")),
-        BottomNavigationBarItem(icon: Icon(Icons.account_balance), title: Text("Third")),
-      ],  onTap: (int index) => debugPrint("Tapped index: $index"),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle), title: Text("Menu")),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.ac_unit), title: Text("Second")),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.account_balance), title: Text("Third")),
+        ],
+        onTap: (int index) => debugPrint("Tapped index: $index"),
       ),
-
       backgroundColor: Colors.redAccent,
       body: Container(
           alignment: Alignment.center,
           child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-
                 CustomButton()
 
                 // InkWell(
@@ -69,5 +70,72 @@ class Home extends StatelessWidget {
             child: Text("Hello Flutter",
                 textDirection: TextDirection.ltr,
                 style: TextStyle(fontWeight: FontWeight.w500))));
+  }
+}
+
+class BizCard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Hi, my name is!",
+          textDirection: TextDirection.ltr,
+        ),
+      ),
+      body: Container(
+        alignment: Alignment.center,
+        child: Stack(
+          alignment: Alignment.topCenter,
+          children: [
+            _getCard(),
+            _getAvatar()
+          ],
+        ),
+      ),
+    );
+  }
+
+  Container _getCard() {
+    return Container(
+      width: 350,
+      height: 200,
+      margin: EdgeInsets.fromLTRB(0, 50, 0, 0),
+      decoration: BoxDecoration(
+        color: Colors.redAccent,
+        borderRadius: BorderRadius.circular(4.5)
+      ),
+
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Zubli Quzaini", 
+          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white)),
+          Text("zubliquzaini@gmail.com"),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(Icons.person_outline),
+              Text("@bedezub")
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Container _getAvatar() {
+    return Container(
+      width: 100,
+      height: 100,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.all(Radius.circular(50.0)),
+        border: Border.all(color: Colors.pinkAccent, width: 2.2),
+        image: DecorationImage(image: NetworkImage("https://picsum.photos/200"),
+        fit: BoxFit.cover
+        )
+      ),
+    );
   }
 }
