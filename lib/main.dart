f33ef41680cc96a0257bd52101fed3b420818180
zubquzaini <zubliquzaini@gmail.com>
2020-08-17 11:20:10 +0800

    Flutter Experimental

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world/parsing_json/json_parsing.dart';
 import 'package:hello_world/ui/home.dart';
 
 void main() => runApp(new MaterialApp(
-  home: BillSplitter()
+  home: JsonParsingSimple()
 ));
 
---
+++ lib/parsing_json/json_parsing.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+
+
+class JsonParsingSimple extends StatefulWidget {
+  JsonParsingSimple({Key key}) : super(key: key);
+
+  @override
+  _JsonParsingSimpleState createState() => _JsonParsingSimpleState();
+}
+
+class _JsonParsingSimpleState extends State<JsonParsingSimple> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+       child: child,
+    );
+  }
+}
